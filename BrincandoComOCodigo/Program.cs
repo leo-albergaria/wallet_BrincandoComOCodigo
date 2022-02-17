97413e930e267a54ded4299c4a9ecbd79a5e9a33
Léo Albergaria <63436406+leo-albergaria@users.noreply.github.com>
2022-02-17 22:51:20 +0000

    Add project files.

---
+++ BrincandoComOCodigo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BrincandoComOCodigo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var contratante = "Eu";
+
+            Console.Read(Contrante);
+
+            Console.WriteLine(contratante);
+        }
+    }
+}
